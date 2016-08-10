80470ca200efae7a94b305576c95fddb23dcd622
Elton da Silva de Moraes <eltonmoraes@ext.cvc.com.br>
2016-08-10 13:50:01 -0300

    Teste integrado

---
+++ src/test/java/br.com.spotippos.challenge/SpotipposIntegrationTest.java
@@ -0,0 +1,34 @@
+package br.com.spotippos.challenge;
+
+import br.com.spotippos.challenge.config.Application;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+/**
+ * Created by mtzcpd663 on 10/08/2016.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {Application.class})
+public class SpotipposIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void testApplicationItsWorking() throws Exception{
+        ResponseEntity<String> entity = testRestTemplate
+                .getForEntity("/spotippos/", String.class);
+
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals(entity.getBody(), "Hello Spotippos");
+    }
+}
