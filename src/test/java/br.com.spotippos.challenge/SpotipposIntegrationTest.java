9da4e8b47b82255dfe90b44187accabe8fabf190
Elton da Silva de Moraes <eltonmoraes@ext.cvc.com.br>
2016-08-11 09:31:37 -0300

    Ajustes nos Testes integrados

--- src/test/java/br.com.spotippos.challenge/SpotipposIntegrationTest.java
+++ src/test/java/br.com.spotippos.challenge/SpotipposIntegrationTest.java
@@ -54,7 +54,6 @@ public class SpotipposIntegrationTest {
 
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         assertTrue(entity.getBody().contains("OK"));
-        assertEquals(entity.getStatusCodeValue(),HttpStatus.OK.value());
     }
 
     @Test
